7ea8cd8037723b1238a08afb7a8a3ada8af65d4e
archerman19 <72265919+archerman19@users.noreply.github.com>
2021-03-17 20:04:39 +0400

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <vector>
 #include <Windows.h>
+#include <conio.h>
+#include <stdio.h>
+
 
 using namespace std;
 
@@ -11,41 +13,41 @@ struct catalog {
 	char helicopter_class[30];
 	char country[30];
 	int year;
-	float price;
-	int capacity;
-	void Print() {
-		cout << mark << "\t\t";
-		cout << "        " << model << "\t";
-		cout << "        " << helicopter_class << "\t";
-		cout << "        " << country << "\t";
-		cout << "        " << year << "\t";
-		cout << "        " << price << "\t";
-		cout << "        " << capacity << "\t";
-		cout << endl;
-	}
+	double price;
 };
 
 catalog hc;
-int start, n = 0;
+
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	ofstream file;
-	cout << "Ввод новой записи - 1" << endl;
-	cout << "Вывод всех записей - 2" << endl;
-	cout << "Поиск - 3" << endl;
-	cout << "Выход - 4" << endl;
-	cout << "Выберите операцию: "; cin >> start;
-	
-	if (start != 4) {
+
+	int start, i;
+	bool find = false;
+	char nameX[40]; // переменная для поиска по строке
+	int choise, numberi;
+	double numberd;
+
+
+	do
+	{
+		system("cls");
+		cout << "Новая запись - 1" << endl;
+		cout << "Просмотр записей - 2" << endl;
+		cout << "Поиск - 3" << endl;
+		cout << "Выход - 4" << endl;
+		cout << "Выберите операцию: "; 
+		cin >> start;
+
 		switch (start)
 		{
 		case 1:
-		{FILE* fa;
-		fa = fopen("file.txt", "w+");
-		if (!fa) {
+		{
+		getchar();
+		ofstream file("file.txt", std::ios::app | ios_base::binary);
+		if (!file.is_open()) {
 			cout << "Файл не открыт!\n";
 		}
 		else {
@@ -55,46 +57,345 @@ int main() {
 			cout << "Страна: "; cin >> hc.country;
 			cout << "Год выпуска: "; cin >> hc.year;
 			cout << "Цена: "; cin >> hc.price;
-			cout << "Вместимость: "; cin >> hc.capacity;
-			fread(&n, sizeof(int), 1, fa);
-			cout << "n = " << n;
-			n++;
-			fwrite(&n, sizeof(int), 1, fa);
-			fclose(fa);
-			FILE* fb;
-			fb = fopen("file.txt", "a");
-			fwrite((char*)&hc, sizeof(catalog), 1, fb);
-			fclose(fb);
-			cout << n;
+			file.write((char*)&hc, sizeof(catalog));
+			file.close();
 		}
+		cout << "Выберите операцию\n";
+		cin >> i;
 		break;
 		}
 		case 2: {
-			FILE* f;
-			f = fopen("file.txt", "r");
-			if (!f) {
+			ifstream file("file.txt");
+			if (!file.is_open()) {
 				cout << "Файл не открыт!\n";
 			}
 			else
 			{
-				fread(&n, sizeof(int), 1, f);
-				cout << n;
-				catalog* p;
-				p = (catalog*)calloc(n, sizeof(catalog));
-				for (int i = 0; i < n; i++) {
-					fread(&p[i], sizeof(catalog), 1, f);
-				}
-				for (int i = 0; i < n; i++) {
-					p[i].Print();
+				cout << "\tВсе записи:\nМарка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+				while (true)
+				{
+
+					file.read((char*)&hc, sizeof(catalog));
+					if (!file.eof())
+						cout << hc.mark << "\t\t" << hc.model << "\t\t"
+						<< hc.helicopter_class << "\t\t" << hc.country
+						<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+
+					else break;
 				}
 			}
-			fclose(f);
+			file.close();
+			cout << "Выберите операцию\n";
+			cin >> i;
 			break;
 		}
 		case 3:
-		default:
-			cout << "error";
-			break;
+			fstream file;
+			int filter_option; // выбор фильтра
+			cout << "Фильтры:\n"
+				<< "\t1 - Поиск по марке\n"
+				<< "\t2 - Поиск по модели\n"
+				<< "\t3 - Поиск по классу\n"
+				<< "\t4 - Поиск по стране\n"
+				<< "\t5 - Поиск по году\n"
+				<< "\t6 - Поиск по цене\n";
+			cout << "Какой фильтр применить? "; 
+			cin >> filter_option;
+			switch (filter_option) {
+			case 1: 
+			{
+				getchar();
+				find = false;
+				cout << "Введите марку вертолета: "; cin.getline(nameX, 40);
+				file.open("file.txt", ios_base::in | ios_base::binary);
+				if (!file.is_open())
+					cout << "Файл не может быть открыт!\n";
+				else {
+					cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+					while (true)
+					{
+						file.read((char*)&hc, sizeof(catalog));
+						if (!file.eof()) {
+							if (strcmp(hc.mark, nameX) == 0)
+							{
+								cout << hc.mark << "\t\t" << hc.model << "\t\t"
+									<< hc.helicopter_class << "\t\t" << hc.country
+									<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+								find = true;
+							}
+						}
+						else
+							break;
+					}
+					system("pause");
+					if (!find)
+						cout << "Записей не найдено.\n";
+					file.close();
+					break;
+				}
+			}
+			case 2:
+				getchar();
+				find = false;
+				cout << "Введите модель вертолета: ";
+				cin.getline(nameX, 40);
+				file.open("file.txt", ios_base::in | ios_base::binary);
+				if (!file.is_open())
+					cout << "Файл не может быть открыт!\n";
+				else {
+					cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+					while (true)
+					{
+						file.read((char*)&hc, sizeof(catalog));
+						if (!file.eof()) {
+							if (strcmp(hc.model, nameX) == 0)
+							{
+								cout << hc.mark << "\t\t" << hc.model << "\t\t"
+									<< hc.helicopter_class << "\t\t" << hc.country
+									<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+								find = true;
+							}
+						}
+						else
+							break;
+					}
+					system("pause");
+					if (!find)
+						cout << "Записей не найдено.\n";
+					file.close();
+					break;
+				}
+			case 3:
+				find = false;
+				getchar();
+				cout << "Введите класс вертолета: "; cin.getline(nameX, 40);
+				file.open("file.txt", ios_base::in | ios_base::binary);
+				if (!file.is_open())
+					cout << "Файл не может быть открыт!\n";
+				else {
+					cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+					while (true)
+					{
+						file.read((char*)&hc, sizeof(catalog));
+						if (!file.eof()) {
+							if (strcmp(hc.helicopter_class, nameX) == 0)
+							{
+								cout << hc.mark << "\t\t" << hc.model << "\t\t"
+									<< hc.helicopter_class << "\t\t" << hc.country
+									<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+								find = true;
+							}
+						}
+						else
+							break;
+					}
+					system("pause");
+					if (!find)
+						cout << "Записей не найдено.\n";
+					file.close();
+					break;
+				}
+			case 4:
+				find = false;
+				getchar();
+				cout << "Введите страну: "; cin.getline(nameX, 40);
+				file.open("file.txt", ios_base::in | ios_base::binary);
+				if (!file.is_open())
+					cout << "Файл не может быть открыт!\n";
+				else {
+					cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+					while (true)
+					{
+						file.read((char*)&hc, sizeof(catalog));
+						if (!file.eof()) {
+							if (strcmp(hc.country, nameX) == 0)
+							{
+								cout << hc.mark << "\t\t" << hc.model << "\t\t"
+									<< hc.helicopter_class << "\t\t" << hc.country
+									<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+								find = true;
+							}
+						}
+						else
+							break;
+					}
+					system("pause");
+					if (!find)
+						cout << "Записей не найдено.\n";
+					file.close();
+					break;
+				}
+			case 5:
+				getchar();
+				int oper; // выбор подфильтра
+				find = false;
+				file.open("file.txt", ios_base::in | ios_base::binary);
+				if (!file.is_open())
+					cout << "Файл не может быть открыт!\n";
+				else {
+					cout << "Выберите операцию: \n1) =\n2) >=\n3) <=\n"; cin >> oper;
+					switch (oper)
+					{
+					case 1:
+						cout << "Введите год: "; cin >> numberi;
+						cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+						while (true)
+						{
+							file.read((char*)&hc, sizeof(catalog));
+							if (!file.eof()) {
+								if (hc.year == numberi)
+								{
+									cout << hc.mark << "\t\t" << hc.model << "\t\t"
+										<< hc.helicopter_class << "\t\t" << hc.country
+										<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+									find = true;
+								}
+							}
+							else
+								break;
+						}
+						system("pause");
+						if (!find)
+							cout << "Записей не найдено.\n";
+						file.close();
+						break;
+					case 2:
+						cout << "Введите год: "; cin >> numberi;
+						cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+						while (true)
+						{
+							file.read((char*)&hc, sizeof(catalog));
+							if (!file.eof()) {
+								if (hc.year >= numberi)
+								{
+									cout << hc.mark << "\t\t" << hc.model << "\t\t"
+										<< hc.helicopter_class << "\t\t" << hc.country
+										<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+									find = true;
+								}
+							}
+							else
+								break;
+						}
+						system("pause");
+						if (!find)
+							cout << "Записей не найдено.\n";
+						file.close();
+						break;
+					case 3:
+						cout << "Введите год: "; cin >> numberi;
+						cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+						while (true)
+						{
+							file.read((char*)&hc, sizeof(catalog));
+							if (!file.eof()) {
+								if (hc.year <= numberi)
+								{
+									cout << hc.mark << "\t\t" << hc.model << "\t\t"
+										<< hc.helicopter_class << "\t\t" << hc.country
+										<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+									find = true;
+								}
+							}
+							else
+								break;
+						}
+						system("pause");
+						if (!find)
+							cout << "Записей не найдено.\n";
+						file.close();
+						break;
+					default:
+						break;
+					}
+				}
+				break;
+			case 6:
+				getchar();
+				find = false;
+				file.open("file.txt", ios_base::in | ios_base::binary);
+				if (!file.is_open())
+					cout << "Файл не может быть открыт!\n";
+				else {
+					cout << "Выберите операцию: \n1) =\n2) >=\n3) <=\n"; cin >> oper; // выбор подфильтра
+					switch (oper) // поиск по подфильтру
+					{
+					case 1: // если введенное значние == заданному
+						cout << "Введите цену: "; cin >> numberd;
+						cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+						while (true)
+						{
+							file.read((char*)&hc, sizeof(catalog));
+							if (!file.eof()) {
+								if (hc.price == numberd)
+								{
+									cout << hc.mark << "\t\t" << hc.model << "\t\t"
+										<< hc.helicopter_class << "\t\t" << hc.country
+										<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+									find = true;
+								}
+							}
+							else
+								break;
+						}
+						system("pause");
+						if (!find)
+							cout << "Записей не найдено.\n";
+						file.close();
+						break;
+					case 2: // если значние >= заданного
+						cout << "Введите цену: "; cin >> numberd;
+						cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+						while (true)
+						{
+							file.read((char*)&hc, sizeof(catalog));
+							if (!file.eof()) {
+								if (hc.price >= numberd)
+								{
+									cout << hc.mark << "\t\t" << hc.model << "\t\t"
+										<< hc.helicopter_class << "\t\t" << hc.country
+										<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+									find = true;
+								}
+							}
+							else
+								break;
+						}
+						system("pause");
+						if (!find)
+							cout << "Записей не найдено.\n";
+						file.close();
+						break;
+					case 3: // если значние <= заданного
+						cout << "Введите цену: "; cin >> numberd;
+						cout << "Марка\t\tМодель\t\tКласс\t\tСтрана\t\tГод выпуска\t\tЦена\n";
+						while (true)
+						{
+							file.read((char*)&hc, sizeof(catalog));
+							if (!file.eof()) {
+								if (hc.price <= numberd)
+								{
+									cout << hc.mark << "\t\t" << hc.model << "\t\t"
+										<< hc.helicopter_class << "\t\t" << hc.country
+										<< "\t\t" << hc.year << "\t\t" << hc.price << endl;
+									find = true;
+								}
+							}
+							else
+								break;
+						}
+						system("pause");
+						if (!find)
+							cout << "Записей не найдено.\n";
+						file.close();
+						break;
+					default:
+						break;
+					}
+				}
+				break;
+
+			}
 		}
-	}
+	} while (start != 4);
 }
